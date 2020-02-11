bd7e29927481ed2d63e8b86b62659f8f0ee827a2
sean <s.warchuck16@gmail.com>
2020-02-11 13:45:22 -0500

    second push

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,12 @@ public class Main {
 
 	public static void main(String[] args) {
 		
+		System.out.println("Please input your name");
+		//find name and set it to the player class
 		
+		System.out.println("What difficulty would you like?");
+		String hangmanWord = Methods.whichDifficulty();
+		System.out.println(hangmanWord);
 		
 	}
 
