3bf12455ba6987021df8473ae5771090102e4a79
billwilt <billwilt@gmail.com>
2020-02-12 17:06:44 -0500

    end of class update

--- src/PlayerScores.java
+++ src/PlayerScores.java
@@ -7,6 +7,7 @@ public class PlayerScores {
 	            
 		    	//clearScreen();
 		    	
+		    	PlayerLineConverter from Line Method
 		    	
 		    	for (int i=0; i < 40; ++i) {
 			    	System.out.println(" ");
