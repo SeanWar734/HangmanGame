7fc15a5837f84c5ad8688d5f097b61195f201189
jillian <jillcenters@protonmail.com>
2020-02-12 15:56:04 -0500

    added all hangperson methods

--- src/HangPerson.java
+++ src/HangPerson.java
@@ -3,58 +3,15 @@ public class HangPerson {
 
 	public static void main(String[] args) {
 		
-		begin();
 	}
-	private String body;
-	 public HangPerson() {
-		 body = "";
-	 }
-	
-	public static void gameOver() {
-		System.out.printf("%28s","=|============|=====\n");
-			  System.out.printf("%9s%14s","|","|\n");
-			  System.out.printf("%9s%14s","|","|\n");
-			  System.out.printf("%11s%12s","(xnx)","|\n");
-			 System.out.printf("%9s%14s","|","|\n");
-			 System.out.printf("%10s%13s","/T\\","|\n");
-			 System.out.printf("%11s%12s","/ | \\","|\n");
-			 System.out.printf("%10s%13s","/*\\","|\n");
-			 System.out.printf("%11s%12s","/   \\","|\n");
-			System.out.printf("%22s\n","|");
-			System.out.printf("%22s\n","|");
-			System.out.printf("%22s\n","|");
-			System.out.printf("%22s\n","|");
-			System.out.println("=======================================");
-				
-	}
+
 	
 	
-	
+	 
 	public static void start() {
-		System.out.printf("%28s","=|============|=====\n");
-			  System.out.printf("%9s%14s","|","|\n");
-			  System.out.printf("%9s%14s","|","|\n");
-			  System.out.printf("%23s","|\n");
-			 System.out.printf("%23s","|\n");
-			 System.out.printf("%23s","|\n");
-			 System.out.printf("%23s","|\n");
-			 System.out.printf("%23s","|\n");
-			 System.out.printf("%23s","|\n");
-			System.out.printf("%22s\n","|");
-			System.out.printf("%22s\n","|");
-			System.out.printf("%22s\n","|");
-			System.out.printf("%22s\n","|");
-			System.out.println("=======================================");
-				
-	}
-	
-	
-	
-	public static void begin() {
 		  System.out.printf("%98s","_\n");
 		  System.out.printf("%47s%52s","_","| |\n");
 		  System.out.printf("%106s","v============|================================================| |======v\n");
-		//  System.out.printf("%34s%14s%50s%8s","|"," | ","| |","|\n");
 		  System.out.printf("%106s","^============|================================================| |======^\n");
 		  System.out.printf("%47s%52s","|","| |\n");		 
 		  System.out.printf("%47s%52s","|","| |\n");		 
@@ -82,10 +39,181 @@ public class HangPerson {
 		  System.out.printf("%99s","| |\n");
 		  System.out.printf("%99s","| |\n");
 		System.out.println("================================================================================================================================================================");
-
-		
-		
-		
+	}
+	
+	
+	
+	public static void oneWrong() {
+		  System.out.printf("%98s","_\n");
+		  System.out.printf("%47s%52s","_","| |\n");
+		  System.out.printf("%106s","v============|================================================| |======v\n");
+		  System.out.printf("%106s","^============|================================================| |======^\n");
+		  System.out.printf("%47s%52s","|","| |\n");		 
+		  System.out.printf("%47s%52s","|","| |\n");		 
+		  System.out.printf("%47s%52s","|","| |\n");		 
+		  System.out.printf("%47s%52s","|","| |\n");		  
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		System.out.println("================================================================================================================================================================");
 	}
 
+	
+	
+	
+	public static void twoWrong() {
+		  System.out.printf("%98s","_\n");
+		  System.out.printf("%47s%52s","_","| |\n");
+		  System.out.printf("%106s","v============|================================================| |======v\n");
+		  System.out.printf("%106s","^============|================================================| |======^\n");
+		  System.out.printf("%47s%52s","|","| |\n");		 
+		  System.out.printf("%47s%52s","|","| |\n");		 
+		  System.out.printf("%47s%52s","|","| |\n");		 
+		  System.out.printf("%47s%52s","|","| |\n");		  
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		System.out.println("================================================================================================================================================================");
+	}
+	
+	
+	
+	public static void threeWrong() {
+		  System.out.printf("%98s","_\n");
+		  System.out.printf("%47s%52s","_","| |\n");
+		  System.out.printf("%106s","v============|================================================| |======v\n");
+		  System.out.printf("%106s","^============|================================================| |======^\n");
+		  System.out.printf("%47s%52s","|","| |\n");		 
+		  System.out.printf("%47s%52s","|","| |\n");		 
+		  System.out.printf("%47s%52s","|","| |\n");		 
+		  System.out.printf("%47s%52s","|","| |\n");		  
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		System.out.println("================================================================================================================================================================");
+	}
+	
+	
+	
+	public static void fourWrong() {
+		  System.out.printf("%98s","_\n");
+		  System.out.printf("%47s%52s","_","| |\n");
+		  System.out.printf("%106s","v============|================================================| |======v\n");
+		  System.out.printf("%106s","^============|================================================| |======^\n");
+		  System.out.printf("%47s%52s","|","| |\n");		 
+		  System.out.printf("%47s%52s","|","| |\n");		 
+		  System.out.printf("%47s%52s","|","| |\n");		 
+		  System.out.printf("%47s%52s","|","| |\n");		  
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		System.out.println("================================================================================================================================================================");
+	}
+	
+	
+	
+	public static void gameOver() {
+		  System.out.printf("%98s","_\n");
+		  System.out.printf("%47s%52s","_","| |\n");
+		  System.out.printf("%106s","v============|================================================| |======v\n");
+		  System.out.printf("%106s","^============|================================================| |======^\n");
+		  System.out.printf("%47s%52s","|","| |\n");		 
+		  System.out.printf("%47s%52s","|","| |\n");		 
+		  System.out.printf("%47s%52s","|","| |\n");		 
+		  System.out.printf("%47s%52s","|","| |\n");		  
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		System.out.println("================================================================================================================================================================");
+	}
 }
