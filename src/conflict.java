19468867e26cf05212ed0db7b18122d2968cd9b4
billwilt <billwilt@gmail.com>
2020-02-12 09:58:27 -0500

    I am adding another conflict

--- src/conflict.java
+++ src/conflict.java
@@ -3,7 +3,7 @@ public class conflict {
 
 	public static void main(String[] args) {
 		
-		System.out.println("fix this");
+		System.out.println("fix this. I do not want to fix this.");
 		System.out.println("I'm creating a conflict. I like to do that.");
 		System.out.println("Jill and Sean are super cool and in my group!");
 		
