45372f0a3a43b87e7952f99f87ae91e03005a336
billwilt <billwilt@gmail.com>
2020-02-12 11:07:40 -0500

    Added high scores class

---
+++ src/HighScores.java
@@ -0,0 +1,60 @@
+		
+		public class HighScores {
+		    public static void main(String args[])
+		        throws InterruptedException {
+	            
+		    	//clearScreen();
+		    	
+		    	
+		    	for (int i=0; i < 40; ++i) {
+			    	System.out.println("                                                                                                    ");
+
+		    	}
+
+	            
+		        String importantInfo[] = {
+		            "                         * * * * * * * * * * * * * THE BEST PLAYERS! * * * * * * * * * * * * *",
+		            "                           RANK           NAME                                     WIN/LOSS",
+		            "                           ------------   --------------------------------------   --------",
+		            " ",
+		            " ",
+		            " ",
+		            " ",
+		            " ",
+		            " ",
+		            " ",
+		            " ",
+		            " ",
+		            " ",
+		            " ",
+		            " ",
+		            " ",
+		            " ",
+		            " ",
+		            " ",
+		            " ",
+		            " ",
+		            " ",
+		            " ",
+		            " ",
+
+		        };
+
+		        for (int i = 0;
+		             i < importantInfo.length;
+		             i++) {
+		            //Pause for 4 seconds
+		            Thread.sleep(300);
+		            //Print a message
+		            System.out.println(importantInfo[i]);
+		        }
+	
+		
+		
+		
+	}
+			public static void clearScreen() {  
+				for (int i = 0; i < 50; ++i) System.out.println();
+			} 
+		    
+}
