29868ae7f416c44b50208380a368531b5208e4b5
billwilt <billwilt@gmail.com>
2020-02-12 09:47:08 -0500

    I am causing a conflict

--- src/conflict.java
+++ src/conflict.java
@@ -4,6 +4,8 @@ public class conflict {
 	public static void main(String[] args) {
 		
 		System.out.println("fix this");
+		System.out.println("I'm creating a conflict. I like to do that.");
+		System.out.println("Jill and Sean are super cool and in my group!");
 		
 	}
 
