6140c0164aa6a650ac198d788fe206ceb327729d
jillian <jillcenters@protonmail.com>
2020-02-12 15:58:11 -0500

    fiveWrong added

--- src/HangPerson.java
+++ src/HangPerson.java
@@ -6,7 +6,6 @@ public class HangPerson {
 	}
 
 	
-	
 	 
 	public static void start() {
 		  System.out.printf("%98s","_\n");
@@ -150,6 +149,41 @@ public class HangPerson {
 	
 	
 	public static void fourWrong() {
+		  System.out.printf("%98s","_\n");
+		  System.out.printf("%47s%52s","_","| |\n");
+		  System.out.printf("%106s","v============|================================================| |======v\n");
+		  System.out.printf("%106s","^============|================================================| |======^\n");
+		  System.out.printf("%47s%52s","|","| |\n");		 
+		  System.out.printf("%47s%52s","|","| |\n");		 
+		  System.out.printf("%47s%52s","|","| |\n");		 
+		  System.out.printf("%47s%52s","|","| |\n");		  
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		  System.out.printf("%99s","| |\n");
+		System.out.println("================================================================================================================================================================");
+	}
+	
+	
+	
+	public static void fiveWrong() {
 		  System.out.printf("%98s","_\n");
 		  System.out.printf("%47s%52s","_","| |\n");
 		  System.out.printf("%106s","v============|================================================| |======v\n");
